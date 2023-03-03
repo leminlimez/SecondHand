f5d13a73045ff8831b378e84ab26deec4b235f52
LeminLimez <59540996+leminlimez@users.noreply.github.com>
2023-03-02 22:27:45 -0500

    Initial Commit

---
+++ SecondHand/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  SecondHand
+//
+//  Created by lemin on 3/2/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SecondHand/SecondHandApp.swift
@@ -0,0 +1,17 @@
+//
+//  SecondHandApp.swift
+//  SecondHand
+//
+//  Created by lemin on 3/2/23.
+//
+
+import SwiftUI
+
+@main
+struct SecondHandApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
